3982bcb3f0fb99e6b8e4b4eadd709e3038bf4430
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-03-16 08:23:32 -0600

    Who put the uppercase W there??

--- src/main/java/world/ntdi/planium/manger/image/Image.java
+++ src/main/java/world/ntdi/planium/manger/image/Image.java
@@ -53,21 +53,4 @@ public class Image {
 
         ImageIO.write(bufferedImage, "png", getFile());
     }
-
-    public enum ImageTypes {
-        STUBBY("src/main/resources/static/base/Ntdi_World_600px-01.png"),
-        LONGER("src/main/resources/static/base/Ntdi_World_1200px-01.png");
-
-        @Getter
-        private final String path;
-
-        ImageTypes(String path) {
-            this.path = path;
-        }
-
-        @Override
-        public String toString() {
-            return getPath();
-        }
-    }
 }

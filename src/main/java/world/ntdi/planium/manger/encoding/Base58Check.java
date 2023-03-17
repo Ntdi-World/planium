3678dd34c5b5560b5fa9403e64b370baeae252b0
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-03-16 19:26:01 -0600

    remove bloat functions

--- src/main/java/world/ntdi/planium/manger/encoding/Base58Check.java
+++ src/main/java/world/ntdi/planium/manger/encoding/Base58Check.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 
 /**
  * Converts between an array of bytes and a Base58Check string. Not instantiable.
+ * Modified by Ntdi
  */
 public final class Base58Check {
 	
@@ -57,51 +58,6 @@ public final class Base58Check {
 			throw new AssertionError(e);
 		}
 	}
-	
-	
-	// Converts the given Base58Check string to a byte array, verifies the checksum, and removes the checksum to return the payload.
-	// The caller is responsible for handling the version byte(s).
-	public static byte[] base58ToBytes(String s) {
-		byte[] concat = base58ToRawBytes(s);
-		byte[] data = Arrays.copyOf(concat, concat.length - 4);
-		byte[] hash = Arrays.copyOfRange(concat, concat.length - 4, concat.length);
-		byte[] rehash = Arrays.copyOf(Sha256.getDoubleHash(data).toBytes(), 4);
-		if (!Arrays.equals(rehash, hash))
-			throw new IllegalArgumentException("Checksum mismatch");
-		return data;
-	}
-	
-	
-	// Converts the given Base58Check string to a byte array, without checking or removing the trailing 4-byte checksum.
-	static byte[] base58ToRawBytes(String s) {
-		// Parse base-58 string
-		BigInteger num = BigInteger.ZERO;
-		for (int i = 0; i < s.length(); i++) {
-			num = num.multiply(ALPHABET_SIZE);
-			int digit = ALPHABET.indexOf(s.charAt(i));
-			if (digit == -1)
-				throw new IllegalArgumentException("Invalid character for Base58Check");
-			num = num.add(BigInteger.valueOf(digit));
-		}
-		
-		// Strip possible leading zero due to mandatory sign bit
-		byte[] b = num.toByteArray();
-		if (b[0] == 0)
-			b = Arrays.copyOfRange(b, 1, b.length);
-		
-		try {
-			// Convert leading '1' characters to leading 0-value bytes
-			ByteArrayOutputStream buf = new ByteArrayOutputStream();
-			for (int i = 0; i < s.length() && s.charAt(i) == ALPHABET.charAt(0); i++)
-				buf.write(0);
-			buf.write(b);
-			return buf.toByteArray();
-		} catch (IOException e) {
-			throw new AssertionError(e);
-		}
-	}
-	
-	
 	
 	/*---- Class constants ----*/
 	
